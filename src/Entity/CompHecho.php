7a5ca8b9e21f85e82ff9945e8e91b81905f6d6a5
Centro Analisis <centrodeanalisischaco@gmail.com>
2021-09-08 10:31:47 -0300

    metodo to string compHecho

--- src/Entity/CompHecho.php
+++ src/Entity/CompHecho.php
@@ -34,6 +34,11 @@ class CompHecho
         $this->presAutors = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->getDescripcion();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
